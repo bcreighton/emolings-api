a3c8e62e2d6d9c55bfe776dbc4cf8bce4078f499
Bryson Creighton <brysoncreighton@gmail.com>
2021-08-13 15:59:56 -0600

    Implement Basic Auth
    Basic API Auth access; no user login

--- src/app.js
+++ src/app.js
@@ -15,9 +15,32 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+// API AUTHORIZATION
+app.use(validateBearerToken = (req, res, next) => {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+  debugger;
+
+  if ( !authToken || authToken.split(' ')[1] !== apiToken ) {
+    debugger;
+    return res.status(401).json({
+      error: 'Unauthorized request'
+    })
+  }
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello, EMOLINGS!')
-})
+});
+
+// app.use('/api/user-type', userTypeRouter);
+// app.use('/api/feeling', feelingRouter);
+// app.use('/api/adv-feeling', advFeelingRouter);
+// app.use('/api/coping-skill', copingSkillRouter);
+// app.use('/api/severity', severityRouter);
+// app.use('/api/identifier', identifierRouter);
+// app.use('/api/reminder', reminderRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response
