dc0cd717a7b67bec19634d021868e07d1db56815
Bryson Creighton <brysoncreighton@gmail.com>
2021-08-14 15:57:55 -0600

    Update adv-feeling-service.js
    Added join request to display main feeling name as opposed to the id. This makes for more concise data when a request is pushed and avoids the need for additional database calls for needed data.

--- src/services/adv-feeling-service.js
+++ src/services/adv-feeling-service.js
@@ -2,20 +2,36 @@ const AdvFeelingService = {
     getAdvFeelings(knex) {
         return knex
             .from('adv_feeling')
-            .select('*')
+            .join('feeling', 'feeling.id', 'adv_feeling.main_feeling')
+            .select(
+                knex.ref('adv_feeling.id').as('id'),
+                knex.ref('adv_feeling.name').as('name'),
+                knex.ref('feeling.name').as('main_feeling')
+            )
     },
 
     getById(knex, id) {
-        return knex
-            .from('adv_feeling')
+        return knex ('adv_feeling')
+            .leftOuterJoin('feeling', 'feeling.id', 'adv_feeling.main_feeling')
             .where('adv_feeling.id', id)
             .first()
+            .select(
+                'adv_feeling.id AS id',
+                'adv_feeling.name AS name',
+                'feeling.name AS main_feeling'
+            )
     },
 
     getByMainFeeling(knex, main_feeling) {
         return knex
             .from('adv_feeling')
+            .leftOuterJoin('feeling', 'feeling.id', 'adv_feeling.main_feeling')
             .where('adv_feeling.main_feeling', main_feeling)
+            .select(
+                'adv_feeling.id AS id',
+                'adv_feeling.name AS name',
+                'feeling.name AS main_feeling'
+            )
     }
 }
 
