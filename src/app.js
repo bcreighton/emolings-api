e1e55c6fd5675555a62258a2e6d1318c78d19dc3
Bryson Creighton <brysoncreighton@gmail.com>
2021-08-16 20:13:13 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -29,10 +29,10 @@ app.use(cors())
 app.use(validateBearerToken = (req, res, next) => {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization');
-  debugger;
+  
 
   if ( !authToken || authToken.split(' ')[1] !== apiToken ) {
-    debugger;
+    
     return res.status(401).json({
       error: 'Unauthorized request'
     })
